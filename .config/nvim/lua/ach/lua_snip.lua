eaa92a2b81fd69117f1a69ce136c1f661e71fcf3
Sami Dj <Mr.Ash_x7@protonmail.ch>
2023-08-17 19:55:06 +0100

    fixing the <TAB> conflict between cmp and lua_snip

--- .config/nvim/lua/ach/lua_snip.lua
+++ .config/nvim/lua/ach/lua_snip.lua
@@ -1,21 +1,21 @@
 local ls = require("luasnip")
 
-
 ls.config.set_config({
-    history=true,
-    updateevents="TextChanged,TextChangedI",
-    enable_autosnippets = true,
-    ext_opt={
-        [require("luasnip.util.types").choiceNode] = {
-            active = {
-                virt_text = {{"●","GruvBoxOrange"}}
-            }
-        }
-    }
-
+	history = true,
+	updateevents = "TextChanged,TextChangedI",
+	delete_check_events = "TextChanged",
+	region_check_events = "CursorMoved",
+	enable_autosnippets = true,
+	ext_opt = {
+		[require("luasnip.util.types").choiceNode] = {
+			active = {
+				virt_text = { { "●", "GruvBoxOrange" } },
+			},
+		},
+	},
 })
 
-require("luasnip.loaders.from_lua").load({paths="~/.config/nvim/snippets"})
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
 --[[ vim.api.nvim_set_keymap("i", "<C-n>", "<Plug>luasnip-next-choice", {}) ]]
 --[[ vim.api.nvim_set_keymap("s", "<C-n>", "<Plug>luasnip-next-choice", {}) ]]
 --[[ vim.api.nvim_set_keymap("i", "<C-p>", "<Plug>luasnip-prev-choice", {}) ]]
