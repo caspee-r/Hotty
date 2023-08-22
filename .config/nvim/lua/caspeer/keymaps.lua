505a28efc4ff70a52d6cdb3110530bf492c4d08a
Sami Dj <Mr.Ash_x7@protonmail.ch>
2023-08-22 15:01:02 +0100

    changing ach to caspeer

---
+++ .config/nvim/lua/caspeer/keymaps.lua
@@ -0,0 +1,134 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- abbreviation
+vim.cmd("ab tel Telescope")
+vim.cmd("ab sm SessionManager")
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+--[[ keymap("n", "<C-h>", "<C-w>h", opts) ]]
+--[[ keymap("n", "<C-j>", "<C-w>j", opts) ]]
+--[[ keymap("n", "<C-k>", "<C-w>k", opts) ]]
+--[[ keymap("n", "<C-l>", "<C-w>l", opts) ]]
+
+-- execute the last commands
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- source file
+vim.keymap.set("n", "<leader><leader>", function()
+	vim.cmd("so")
+end)
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+--
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
+-- split vertically and show the next buffer
+--[[ keymap("n", "<C-V>", ":vsplit ") ]]
+
+-- switch to zen mode
+--[[ keymap("n", "<leader>z", ":ZenMode<CR>", opts) ]]
+
+-- vim paste whate you yank not delete
+keymap("n", ",p", '"0p', {})
+keymap("n", ",P", '"0P', {})
+
+-- telescope abbr
+--[[ keymap("n", "tl", ":Telescope<CR>", opts) ]]
+
+-- lsp formatting
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- Navigate buffers
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Telescope
+keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
+keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
+keymap("n", "<leader>fc", "<cmd>Telescope current_buffer_fuzzy_find<CR>", opts)
+keymap("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", opts)
+keymap("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", opts)
+
+-- Bufferline
+-- cycling
+keymap("n", "<A-l>", ":BufferLineCycleNext<CR>", opts)
+keymap("n", "<A-h>", ":BufferLineCyclePrev<CR>", opts)
+-- go to buffer
+keymap("n", "g1", ":BufferLineGoToBuffer 1<CR>", opts)
+keymap("n", "g2", ":BufferLineGoToBuffer 2<CR>", opts)
+keymap("n", "g3", ":BufferLineGoToBuffer 3<CR>", opts)
+keymap("n", "g4", ":BufferLineGoToBuffer 4<CR>", opts)
+keymap("n", "g5", ":BufferLineGoToBuffer 5<CR>", opts)
+keymap("n", "g6", ":BufferLineGoToBuffer 6<CR>", opts)
+-- pick buffer
+keymap("n", "<leader>bc", ":BufferLinePickClose<CR>", opts)
+-- toggle pin buffer
+keymap("n", "<leader>bp", ":BufferLineTogglePin<CR>", opts)
+keymap("n", "<leader>br", ":BufferLineCloseRight<CR>", opts)
+keymap("n", "<leader>bl", ":BufferLineCloseLeft<CR>", opts)
+
+-- Neo-tree
+--[[ keymap("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts) ]]
+-- Visual --
+--  Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Command
+keymap("c", ":SessionManager load_last_session<CR>", ":SessionManager<CR>", { silent = false })
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+--Trouble--
+keymap("n", "<leader>q", "<cmd>TroubleToggle<CR>", opts)
+
+-- Knap
+-- set shorter name for keymap function
---
+++ .config/nvim/lua/caspeer/lua_snip.lua
@@ -0,0 +1,22 @@
+local ls = require("luasnip")
+
+ls.config.set_config({
+	history = true,
+	updateevents = "TextChanged,TextChangedI",
+	delete_check_events = "TextChanged",
+	region_check_events = "CursorMoved",
+	enable_autosnippets = true,
+	ext_opt = {
+		[require("luasnip.util.types").choiceNode] = {
+			active = {
+				virt_text = { { "●", "GruvBoxOrange" } },
+			},
+		},
+	},
+})
+
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
+--[[ vim.api.nvim_set_keymap("i", "<C-n>", "<Plug>luasnip-next-choice", {}) ]]
+--[[ vim.api.nvim_set_keymap("s", "<C-n>", "<Plug>luasnip-next-choice", {}) ]]
+--[[ vim.api.nvim_set_keymap("i", "<C-p>", "<Plug>luasnip-prev-choice", {}) ]]
+--[[ vim.api.nvim_set_keymap("s", "<C-p>", "<Plug>luasnip-prev-choice", {}) ]]
