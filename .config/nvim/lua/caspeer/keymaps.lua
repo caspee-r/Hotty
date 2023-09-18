0e78a0769c80ea274538691a97d7a8c9473dce3c
Sami Dj <Mr.Ash_x7@protonmail.ch>
2023-09-18 02:53:52 +0100

    keymaping from the moon

--- .config/nvim/lua/caspeer/keymaps.lua
+++ .config/nvim/lua/caspeer/keymaps.lua
@@ -2,6 +2,7 @@ local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
 
+
 -- abbreviation
 vim.cmd("ab tel Telescope")
 vim.cmd("ab sm SessionManager")
@@ -11,7 +12,24 @@ local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
+keymap("n", "<leader>e", ":Lexplore<CR>", term_opts)
 
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+
+
+
+-- a template for text objects
+--vim.cmd [[
+--xnoremap <silent> i, :<c-u>normal! T,vt,<cr>
+--onoremap <silent> i, :<c-u>normal! T,vt,<cr>
+--xnoremap <silent> a, :<c-u>normal! F,ft,<cr>
+--onoremap <silent> a, :<c-u>normal! F,ft,<cr>
+--]]
+--
+--[[ vim.cmd [[ ]]
+--[[ map <C-H> <Nop> ]]
+--[[ ]]
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
@@ -40,7 +58,7 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- source file
 vim.keymap.set("n", "<leader><leader>", function()
-	vim.cmd("so")
+    vim.cmd("so")
 end)
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
@@ -52,26 +70,50 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<C-d>", "<C-d>zz", opts)
 keymap("n", "<C-u>", "<C-u>zz", opts)
 
--- split vertically and show the next buffer
---[[ keymap("n", "<C-V>", ":vsplit ") ]]
 
--- switch to zen mode
---[[ keymap("n", "<leader>z", ":ZenMode<CR>", opts) ]]
+-- buffer cycling
+keymap("n", "<A-l>", ":bn<CR>", opts)
+keymap("n", "<A-h>", ":bp<CR>", opts)
+
+
+
+--
+
+--vim.cmd [[nmap <S-Enter> O<Esc>j
+--nmap <CR> o<Esc>k
+--]]
+vim.cmd [[
+noremap <leader>; q:is:::g<Left><Left><Left>
+noremap <leader>% q:i%s:::g<Left><Left><Left>
+noremap <leader>" q:i%s:::cg<Left><Left><Left><Left>
+]]
+
+--vim.cmd [[
+--noremap : q:i
+--]]
 
 -- vim paste whate you yank not delete
 keymap("n", ",p", '"0p', {})
 keymap("n", ",P", '"0P', {})
 
--- telescope abbr
---[[ keymap("n", "tl", ":Telescope<CR>", opts) ]]
+-- alternate file
+keymap("n", "<leader>m", ":e #<CR>", opts)
+
+-- quickfix list
+keymap("n", "<leader>co", ":copen<CR>", opts)
+keymap("n", "<leader>cc", ":cclose<CR>", opts)
+keymap("n", "[c", ":cprev<CR>", opts)
+keymap("n", "]c", ":cnext<CR>", opts)
+
 
 -- lsp formatting
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.references)
 -- Navigate buffers
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 
 -- Telescope
 keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
@@ -81,46 +123,20 @@ keymap("n", "<leader>fc", "<cmd>Telescope current_buffer_fuzzy_find<CR>", opts)
 keymap("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", opts)
 keymap("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", opts)
 
--- Bufferline
--- cycling
-keymap("n", "<A-l>", ":BufferLineCycleNext<CR>", opts)
-keymap("n", "<A-h>", ":BufferLineCyclePrev<CR>", opts)
--- go to buffer
-keymap("n", "g1", ":BufferLineGoToBuffer 1<CR>", opts)
-keymap("n", "g2", ":BufferLineGoToBuffer 2<CR>", opts)
-keymap("n", "g3", ":BufferLineGoToBuffer 3<CR>", opts)
-keymap("n", "g4", ":BufferLineGoToBuffer 4<CR>", opts)
-keymap("n", "g5", ":BufferLineGoToBuffer 5<CR>", opts)
-keymap("n", "g6", ":BufferLineGoToBuffer 6<CR>", opts)
--- pick buffer
-keymap("n", "<leader>bc", ":BufferLinePickClose<CR>", opts)
--- toggle pin buffer
-keymap("n", "<leader>bp", ":BufferLineTogglePin<CR>", opts)
-keymap("n", "<leader>br", ":BufferLineCloseRight<CR>", opts)
-keymap("n", "<leader>bl", ":BufferLineCloseLeft<CR>", opts)
-
--- Neo-tree
---[[ keymap("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts) ]]
 -- Visual --
 --  Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+--keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+--keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 
--- Command
-keymap("c", ":SessionManager load_last_session<CR>", ":SessionManager<CR>", { silent = false })
--- Terminal --
 -- Better terminal navigation
 keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
@@ -130,5 +146,15 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 --Trouble--
 keymap("n", "<leader>q", "<cmd>TroubleToggle<CR>", opts)
 
--- Knap
--- set shorter name for keymap function
+vim.cmd [[
+function! ZoteroCite()
+  let format = &filetype =~ '.*tex' ? 'citep' : 'pandoc'
+  let api_call = 'http://127.0.0.1:23119/better-bibtex/cayw?format='.format.'&brackets=1'
+  let ref = system('curl -s '.shellescape(api_call))
+  return ref
+endfunction
+
+noremap <leader>tz "=ZoteroCite()<CR>p
+inoremap <C-z> <C-r>=ZoteroCite()<CR>
+
+]]
