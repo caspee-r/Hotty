bbc471969850ef17c38d8b6a0045750e55950f03
Sami Dj <Mr.Ash_x7@protonmail.ch>
2023-09-18 02:59:45 +0100

    removing unwanted extensions

--- .config/nvim/lua/caspeer/treesitter.lua
+++ .config/nvim/lua/caspeer/treesitter.lua
@@ -8,10 +8,6 @@ configs.setup({
 	ensure_installed = { "python", "lua", "c", "bash", "rust", "markdown", "markdown_inline", "html", "css", "comment" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
-	autotag = {
-		enable = true,
-		filetype = { "html", "xml", "python" },
-	},
 	autopairs = {
 		enable = true,
 	},
@@ -20,7 +16,7 @@ configs.setup({
 		disable = { "" }, -- list of language that will be disabled
 		additional_vim_regex_highlighting = false,
 	},
-	indent = { enable = false },
+	indent = { enable = true },
 	context_commentstring = {
 		enable = true,
 		enable_autocmd = true,
@@ -33,107 +29,66 @@ configs.setup({
 		-- colors = {}, -- table of hex strings
 		-- termcolors = {} -- table of colour name strings
 		--
-	},
-	playground = {
-		enable = true,
-		disable = {},
-		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-		persist_queries = false, -- Whether the query persists across vim sessions
-		keybindings = {
-			toggle_query_editor = "o",
-			toggle_hl_groups = "i",
-			toggle_injected_languages = "t",
-			toggle_anonymous_nodes = "a",
-			toggle_language_display = "I",
-			focus_language = "f",
-			unfocus_language = "F",
-			update = "R",
-			goto_node = "<cr>",
-			show_help = "?",
-		},
-	},
-})
+    },
+    textobjects = {
+        select = {
+            enable = true,
 
-local status_ok, context = pcall(require, "treesitter-context")
-if not status_ok then
-	return
-end
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
 
-context.setup({
-	enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-	max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
-	trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-	min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-	patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-		-- For all filetypes
-		-- Note that setting an entry here replaces all other patterns for this entry.
-		-- By setting the 'default' entry below, you can control which nodes you want to
-		-- appear in the context window.
-		default = {
-			"class",
-			"function",
-			"method",
-			"for",
-			"while",
-			"if",
-			"switch",
-			"case",
-		},
-		-- Patterns for specific filetypes
-		-- If a pattern is missing, *open a PR* so everyone can benefit.
-		tex = {
-			"chapter",
-			"section",
-			"subsection",
-			"subsubsection",
-		},
-		rust = {
-			"impl_item",
-			"struct",
-			"enum",
-		},
-		scala = {
-			"object_definition",
-		},
-		vhdl = {
-			"process_statement",
-			"architecture_body",
-			"entity_declaration",
-		},
-		markdown = {
-			"section",
-		},
-		elixir = {
-			"anonymous_function",
-			"arguments",
-			"block",
-			"do_block",
-			"list",
-			"map",
-			"tuple",
-			"quoted_content",
-		},
-		json = {
-			"pair",
-		},
-		yaml = {
-			"block_mapping_pair",
-		},
-	},
-	exact_patterns = {
-		-- Example for a specific filetype with Lua patterns
-		-- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-		-- exactly match "impl_item" only)
-		-- rust = true,
-	},
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                -- nvim_buf_set_keymap) which plugins like which-key display
+                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                -- You can also use captures from other query groups like `locals.scm`
+                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+                ['@parameter.outer'] = 'v', -- charwise
+                ['@function.outer'] = 'V', -- linewise
+                ['@class.outer'] = '<c-v>', -- blockwise
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true of false
+            include_surrounding_whitespace = true,
+        },
+    },
 
-	-- [!] The options below are exposed but shouldn't require your attention,
-	--     you can safely ignore them.
-
-	zindex = 20, -- The Z-index of the context window
-	mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-	-- Separator between context and content. Should be a single character string, like '-'.
-	-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-	separator = nil,
-	on_attach = nil,
+playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+        toggle_query_editor = "o",
+        toggle_hl_groups = "i",
+        toggle_injected_languages = "t",
+        toggle_anonymous_nodes = "a",
+        toggle_language_display = "I",
+        focus_language = "f",
+        unfocus_language = "F",
+        update = "R",
+        goto_node = "<cr>",
+        show_help = "?",
+    },
+},
 })
