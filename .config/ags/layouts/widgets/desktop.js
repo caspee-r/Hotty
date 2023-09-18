d18d81320574adcaa350c26b0132e28fe9163b3e
Sami Dj <Mr.Ash_x7@protonmail.ch>
2023-09-18 03:01:54 +0100

    tweaking ags

--- .config/ags/layouts/widgets/desktop.js
+++ .config/ags/layouts/widgets/desktop.js
@@ -77,45 +77,45 @@ Widget.widgets['desktop'] = props => Widget({
             box.valign = imports.gi.Gtk.Align[valign.toUpperCase()];
             box.setStyle(`margin: ${Number(offset)}px;`);
         }]],
-        children: [
-            {
-                type: 'box',
-                className: 'clock-box-shadow',
-                children: [{
-                    type: 'centerbox',
-                    className: 'clock-box',
-                    children: [
-                        {
-                            type: 'clock',
-                            className: 'clock',
-                            halign: 'center',
-                            format: '%H',
-                        },
-                        {
-                            type: 'box',
-                            className: 'separator-box',
-                            orientation: 'vertical',
-                            hexpand: true,
-                            halign: 'center',
-                            children: [
-                                { type: 'separator', valign: 'center', vexpand: true },
-                                { type: 'separator', valign: 'center', vexpand: true },
-                            ],
-                        },
-                        {
-                            type: 'clock',
-                            halign: 'center',
-                            className: 'clock',
-                            format: '%M',
-                        },
-                    ],
-                }],
-            },
-            {
-                type: 'clock',
-                className: 'date',
-                format: '%B %e. %A',
-            },
-        ],
+        /* children: [ */
+        /*     { */
+        /*         type: 'box', */
+        /*         className: 'clock-box-shadow', */
+        /*         children: [{ */
+        /*             type: 'centerbox', */
+        /*             className: 'clock-box', */
+        /*             children: [ */
+        /*                 { */
+        /*                     type: 'clock', */
+        /*                     className: 'clock', */
+        /*                     halign: 'center', */
+        /*                     format: '%H', */
+        /*                 }, */
+        /*                 { */
+        /*                     type: 'box', */
+        /*                     className: 'separator-box', */
+        /*                     orientation: 'vertical', */
+        /*                     hexpand: true, */
+        /*                     halign: 'center', */
+        /*                     children: [ */
+        /*                         { type: 'separator', valign: 'center', vexpand: true }, */
+        /*                         { type: 'separator', valign: 'center', vexpand: true }, */
+        /*                     ], */
+        /*                 }, */
+        /*                 { */
+        /*                     type: 'clock', */
+        /*                     halign: 'center', */
+        /*                     className: 'clock', */
+        /*                     format: '%M', */
+        /*                 }, */
+        /*             ], */
+        /*         }], */
+        /*     }, */
+        /*     { */
+        /*         type: 'clock', */
+        /*         className: 'date', */
+        /*         format: '%B %e. %A', */
+        /*     }, */
+        /* ], */
     },
 });
