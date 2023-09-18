c9be939324a820fdc13ce978a5d13a80f00c8d5a
Sami Dj <Mr.Ash_x7@protonmail.ch>
2023-09-18 03:11:13 +0100

    tweaking this plugin

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,36 +1,13 @@
 return {
-	"ThePrimeagen/harpoon",
-	opts = {
-		global_settings = {
-			-- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-			save_on_toggle = true,
-
-			-- saves the harpoon file upon every change. disabling is unrecommended.
-			save_on_change = true,
-
-			-- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-			enter_on_sendcmd = false,
-
-			-- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-			tmux_autoclose_windows = false,
-
-			-- filetypes that you want to prevent from adding to the harpoon list menu.
-			excluded_filetypes = { "harpoon" },
-
-			-- set marks specific to each git branch inside git repository
-			mark_branch = false,
-		},
-	},
-	keys = {
-		{ "<leader>m", "<cmd>:lua require('harpoon.mark').add_file()<CR>", desc = "harpoon" },
-		{ "<leader>h", "<cmd>:lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "harpoon" },
-		{ "<A-1>", '<cmd>:lua require("harpoon.ui").nav_file(1)<CR>' },
-		{ "<A-2>", '<cmd>:lua require("harpoon.ui").nav_file(2)<CR>' },
-		{ "<A-3>", '<cmd>:lua require("harpoon.ui").nav_file(3)<CR>' },
-		{ "<A-4>", '<cmd>:lua require("harpoon.ui").nav_file(4)<CR>' },
-		{ "<A-5>", '<cmd>:lua require("harpoon.ui").nav_file(5)<CR>' },
-		{ "<A-6>", '<cmd>:lua require("harpoon.ui").nav_file(6)<CR>' },
-		{ "<A-7>", '<cmd>:lua require("harpoon.ui").nav_file(7)<CR>' },
-		{ "<A-8>", '<cmd>:lua require("harpoon.ui").nav_file(8)<CR>' },
-	},
+    "ThePrimeagen/harpoon",
+    opts = {},
+    keys = {
+        {"<leader>h",':lua require("harpoon.ui").toggle_quick_menu()<CR>',silent = true},
+        {"<leader>'",':lua require("harpoon.mark").add_file()<CR>',silent = true},
+        {"<A-1>",':lua require("harpoon.ui").nav_file(1)<CR>',silent = true},
+        {"<A-2>",':lua require("harpoon.ui").nav_file(2)<CR>',silent = true},
+        {"<A-3>",':lua require("harpoon.ui").nav_file(3)<CR>',silent = true},
+        {"<A-4>",':lua require("harpoon.ui").nav_file(4)<CR>',silent = true},
+    }
 }
+
--- .config/nvim/lua/plugins/lsp_saga.lua
+++ .config/nvim/lua/plugins/lsp_saga.lua
@@ -46,7 +46,7 @@ return {
         -- It also supports open/vsplit/etc operations, do refer to "definition_action_keys"
         -- It also supports tagstack
         -- Use <C-t> to jump back
-        keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
+        keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
 
         -- Go to definition
         keymap("n","gd", "<cmd>Lspsaga goto_definition<CR>")
@@ -106,8 +106,8 @@ return {
         --[[ keymap("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>") ]]
 
         -- Call hierarchy
-        keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
-        keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+        -- keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+        -- keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
 
         -- Floating terminal
         keymap({"n", "t"}, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
